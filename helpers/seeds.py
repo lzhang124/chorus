94663016afbf174533a798452248c6057156b822
Spencer Chang <spencerc99@gmail.com>
2017-09-16 17:44:16 -0400

    added a helper to seed the databse

---
+++ helpers/seeds.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['app']
+
+songs = []
+N_SONGS = 3
+for j in range(N_SONGS):
+    songs.append({"measures": ["0b" + "0" * 12 + "1" * 13] * j, "num_measures": j})
+db['songs'].insert_many(songs)
+
+users = []
+N_USERS = 3
+for i in range(N_USERS):
+    song = db['songs'].insert_one({"measures": "0b" + "0" * 12 + "1" * 13, "num_measures": 1}).inserted_id
+    users.append({"user": "user" + i, "contributed": {song: "1"}})
+
+db['users'].insert_many(users)
