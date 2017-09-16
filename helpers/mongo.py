43c478bc9e139c0a6e5c74c9ad8c0efc0c5b0ae9
Joshua Jacob <jacobj10@bbhcsd.org>
2017-09-16 17:29:19 -0400

    added more routes

--- helpers/mongo.py
+++ helpers/mongo.py
@@ -9,7 +9,7 @@ import random
 client = MongoClient('localhost', 27017)
 db = client['app']
 
-def update_song(user_id, measure, song_id=None):
+def update_song(ip, measure, song_id):
     measure_num = 0
     if not song_id:
         song_id = db['songs'].insert_one({"measures": [measure], "num_measures": 1}).inserted_id
@@ -19,13 +19,15 @@ def update_song(user_id, measure, song_id=None):
                                          '$push': {'measures': measure}})['num_measures']
 
     # asumes "contributed" is another collection
-    db['users'].find_one({'_id': user_id})["contributed"][song_id] = measure
-
+    db['users'].find_one({'ip': ip})["contributed"][song_id] = measure
 
 def get_song(ip):
     user_contributed = db['users'].find_one({'ip': ip})["contributed"]
     valid_songs = [song['_id'] for song in db['songs'].find({}) if song['_id'] not in user_contributed.values()]    
-    return random.choice(valid_songs if valid_songs != [] else [-1,])
+    choice = random.choice(valid_songs if valid_songs != [] else [-1,])
+    if choice != -1:
+    	choice = str(choice)
+    return choice
 
 def auth(ip):
 	exists = db['users'].find_one({
@@ -36,3 +38,9 @@ def auth(ip):
 			'ip': ip,
 			'contributed': {}
 		})
+
+def get_songs():
+	return db['songs'].find({})
+
+def get_users():
+	return db['users'].find({})
--- routes.py
+++ routes.py
@@ -1,17 +1,34 @@
 from flask import session, request, render_template, jsonify, redirect
 from app import app
 
-from helpers.mongo import get_song, auth, update_song
+from helpers.mongo import get_songs, auth, update_song, get_song
 
 @app.route('/')
 def index_view():
   return render_template('index.html')
 
-@app.route('/api/get_song')
-def song_id():
+@app.route('/api/get_random')
+def get_random():
 	output = get_song(request.remote_addr)
 	success = 0 if output != -1 else 1
 	return jsonify(
 		code=success,
 		response=output
 	)
+
+@app.route('/api/update', methods=['POST',])
+def update():
+	content = request.json
+	_id = None
+	if 'song_id' in content.keys():
+		_id = content['song_id']
+	try:
+		update_song(request.remote_addr, content['measure'], _id)
+		code = 0
+	except:
+		code = 1
+	finally:
+		return jsonify(
+			code=code
+		)
+
