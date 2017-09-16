25bd8b7f5593fdd7e2b10d7e13c784df8dac7519
Joshua Jacob <jacobj10@bbhcsd.org>
2017-09-16 16:03:19 -0400

    added auth

--- helpers/mongo.py
+++ helpers/mongo.py
@@ -2,3 +2,16 @@ from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client['app']
+
+def auth(ip):
+	exists = db['users'].find_one({
+		'ip': ip
+	})
+	if exists:
+		uiud = exists['_id']
+	else:
+		uiud = db['users'].insert({
+			'ip': ip,
+			'contributed' {}
+		})
+	return 0
