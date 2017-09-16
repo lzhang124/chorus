906283ae6fe36227750ec088983113d2f6dcdf59
Larry Zhang <lzhang124@gmail.com>
2017-09-16 19:14:19 -0400

    notes mapping

--- static/js/main.js
+++ static/js/main.js
@@ -65,6 +65,12 @@ function dotClick(d) {
 
 var synth = new Tone.PolySynth().toMaster()
 
+var NOTES = ['C5', 'B4', 'A#4', 'A4', 'G#4', 'G4', 'F#4', 'F4', 'E4', 'D#4',
+             'D4', 'C#4', 'C4', 'B3', 'A#3', 'A3', 'G#3', 'G3', 'F#3', 'F3',
+             'E3', 'D#3', 'D3', 'C#3', 'C3']
+
+// synth.triggerAttackRelease('C4', '8n')
+
 function encode(notes) {
   enc_notes = new Array(N_COLS);
   for (x in notes) {
