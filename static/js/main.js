163336c5d2bd67f883ecef7ad70419bd61b552a1
Larry Zhang <lzhang124@gmail.com>
2017-09-16 20:36:46 -0400

    spencer commits literal trash

--- static/js/main.js
+++ static/js/main.js
@@ -69,10 +69,6 @@ var NOTES = ['C5', 'B4', 'A#4', 'A4', 'G#4', 'G4', 'F#4', 'F4', 'E4', 'D#4',
              'D4', 'C#4', 'C4', 'B3', 'A#3', 'A3', 'G#3', 'G3', 'F#3', 'F3',
              'E3', 'D#3', 'D3', 'C#3', 'C3']
 
-NOTES = NOTES.reverse();
-
-// synth.triggerAttackRelease(['C4', 'C3'], '8n')
-
 function encode(notes) {
   enc_notes = new Array(N_COLS);
   for (i = 0; i < N_COLS; i++) {
