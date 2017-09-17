d988ec2efdf29e69c697cbc14e77b1a82786b7ce
Joshua Jacob <jacobj10@bbhcsd.org>
2017-09-17 01:43:05 -0400

    added location support

--- helpers/seeds.py
+++ helpers/seeds.py
@@ -9,14 +9,12 @@ db.users.remove()
 songs = []
 N_SONGS = 3
 MEASURE = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [2, 0.5], [32, 2], [1024, 32], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]]
-for j in range(N_SONGS):
-    songs.append({"measures": MEASURE, "num_measures": 1})
-db['songs'].insert_many(songs)
+coords = [(1.0, 0.0),]
 
 users = []
 N_USERS = 3
 for i in range(N_USERS):
-    song = db['songs'].insert_one({"measures": MEASURE, "num_measures": 1}).inserted_id
+    song = db['songs'].insert_one({"measures": MEASURE, "locations": coords, "num_measures": 1}).inserted_id
     users.append({"user": "user" + str(i), "contributed": {str(song): "1"}})
 
 db['users'].insert_many(users)
